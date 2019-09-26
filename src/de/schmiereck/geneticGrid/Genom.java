e361488d077b0ca10c870f28ff2558ed40ca4a02
smk@o4b.de <smk@o4b.de>
2019-09-26 15:53:38 +0200

    Intial.

---
+++ src/de/schmiereck/geneticGrid/Genom.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) open4business GmbH, 2019
+ */
+package de.schmiereck.geneticGrid;
+
+/**
+ * <p>
+ *	Genom of a {@link Cell}.
+ * </p>
+ * 
+ * @author smk
+ * @version <p>26.09.2019:	created, smk</p>
+ */
+public class Genom
+{
+
+}
---
+++ src/de/schmiereck/geneticGrid/GridCell.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) open4business GmbH, 2019
+ */
+package de.schmiereck.geneticGrid;
+
+/**
+ * <p>
+ *	Grid-Cell of a {@link Grid}.
+ * </p>
+ * 
+ * @author smk
+ * @version <p>26.09.2019:	created, smk</p>
+ */
+public class GridCell
+{
+	private final GridCell[] next = new GridCell[4];
+	
+	private Cell cell = null;
+
+	public Cell getCell()
+	{
+		return this.cell;
+	}
+
+	public void setCell(Cell cell)
+	{
+		this.cell = cell;
+	}
+	
+	public GridCell getNextGridCell(final int orientation)
+	{
+		return this.next[orientation];
+	}
+	
+	public void setNextGridCell(final int orientation, final GridCell gridCell)
+	{
+		this.next[orientation] = gridCell;
+	}
+
+}
